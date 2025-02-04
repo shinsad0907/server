b814296199f6d69b411a78f146964830ebe652f8
shinsad0907 <112173054+shinsad0907@users.noreply.github.com>
2025-02-04 23:29:28 +0800

    Update serverattack.py

--- serverattack.py
+++ serverattack.py
@@ -1,23 +1,14 @@
 import asyncio
 import websockets
 
-clients = set()  # Set để lưu trữ tất cả các client kết nối
-
 async def handler(websocket, path):
     print("📡 Client connected!")
-    clients.add(websocket)
     try:
         async for message in websocket:
             print(f"📩 Received: {message}")
-            # Gửi lại cho tất cả các client khác
-            for client in clients:
-                if client != websocket:
-                    await client.send(f"🛠️ Server: {message}")
             await websocket.send(f"🛠️ Server: {message}")
     except websockets.exceptions.ConnectionClosed:
         print("❌ Client disconnected!")
-    finally:
-        clients.remove(websocket)
 
 start_server = websockets.serve(handler, "0.0.0.0", 5000)
 
